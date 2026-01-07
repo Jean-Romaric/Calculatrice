b177aa296fad3af2e9dc9740f7e5b7d7c6cf24fe
Jean-Romaric <romaric.akpoue@epitech.eu>
2026-01-07 05:00:05 +0000

    display number

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() { 
 //alert("bonjour")
     let screen = document.querySelector(".screen");
-  //screen.textContent;
+  screen.textContent="";
 
 
   let button = document.querySelectorAll(".button");
-    
+//console.log(button);
+button.forEach((element, index, array) => {
+   console.log(element.innerText);
+     let nbre = null;
+     element.addEventListener("click",()=>{
+      nbre = element.innerText;
+      screen.textContent = screen.textContent + nbre;
+     })
+});    
 
-  let nbre = 7;
-  button[0].addEventListener("click",()=>{
-    screen.textContent += nbre;
-  });
-
+  //let nbre = "7";
+  //button[0].addEventListener("click",()=>{
+    //screen.textContent += nbre;
+  //});
   
+ //let a = 2;
+ //let b = null;
+  
+ //b += a;//b = b + a;
+ //console.log(b);
+
+ //b += a;//b = b + a;
+ ///console.log(b);
+
+ //console.log(""+ 13);
  
+  //let nombre = "8";
+  //button[1].addEventListener("click",()=>{
+    //screen.textContent += nombre;
+  //});
+  
  });
