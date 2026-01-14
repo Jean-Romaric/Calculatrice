d39928641f9be38bb5b065444aa8023268d70ce5
Jean-Romaric <romaric.akpoue@epitech.eu>
2026-01-14 16:18:22 +0000

    prevent_double_operators

--- script.js
+++ script.js
@@ -1,43 +1,112 @@
-document.addEventListener('DOMContentLoaded', function() { 
+document.addEventListener('DOMContentLoaded', function() {
 //alert("bonjour")
     let screen = document.querySelector(".screen");
   screen.textContent="";
 
 
-  let button = document.querySelectorAll(".button");
-//console.log(button);
-     //let nbre = null;
-     let chain_cara = [];
+  let buttons = document.querySelectorAll(".button");
+  let opperateur =  document.getElementsByClassName("opperateur");
 
-button.forEach((element, index, array) => {
-   //console.log(element.innerText);
-     element.addEventListener("click",()=>{
-      nbre = element.innerText;
-      chain_cara.push(nbre);
-      console.log(chain_cara.join(''));
-      screen.textContent = chain_cara.join('').slice(-16);
-     })
-});    
 
-  //let nbre = "7";
-  //button[0].addEventListener("click",()=>{
-    //screen.textContent += nbre;
-  //});
-  
- //let a = 2;
- //let b = null;
-  
- //b += a;//b = b + a;
- //console.log(b);
+  let division = opperateur[0].textContent;
+  let multiplication = opperateur[1].textContent;
+  let moins = opperateur[2].textContent;
+  let plus = opperateur[3].textContent;
 
- //b += a;//b = b + a;
- ///console.log(b);
 
- //console.log(""+ 13);
- 
-  //let nombre = "8";
-  //button[1].addEventListener("click",()=>{
-    //screen.textContent += nombre;
-  //});
-  
+  let memoire = [];
+
+
+  buttons.forEach((button, index, array) => {
+      button.addEventListener("click",(event)=>{
+        nbre = button.innerText;
+        
+
+
+         //if(memoire[memoire.length - 1] == "-" || memoire[memoire.length - 1] =="x" || memoire[memoire.length - 1] =="+" || memoire[memoire.length - 1] == "/"){
+          // memoire.pop()
+        //};
+
+
+        //console.log(memoire[memoire.length - 1] == "-" || memoire[memoire.length - 1] =="x" || memoire[memoire.length - 1] =="+" || memoire[memoire.length - 1] == "/")
+        //console.log(division);
+
+
+
+        if (memoire.length === 0 )  {
+            if(button.classList.contains("opperateur")
+               && !button.classList.contains("moins")
+               || button.classList.contains("vircule")
+              ){
+              return
+            }else{
+              memoire.push(nbre);
+              console.log(memoire);
+            }
+        }else{
+          
+          if(memoire.includes("+") && memoire[memoire.length-1] == "+" && event.target.classList.contains("opperateur")){
+            return
+          } else if(memoire.includes("-") && memoire[memoire.length-1] == "-" &&  event.target.classList.contains("opperateur")) {
+             return
+          } else if (memoire.includes("x") && memoire[memoire.length-1] == "x" &&  event.target.classList.contains("opperateur")) {
+            return
+          }else if(memoire.includes("/") && memoire[memoire.length-1] == "/" &&  event.target.classList.contains("opperateur")) {
+            return
+          }else {
+            memoire.push(nbre);
+           console.log(memoire);
+          }
+
+
+
+
+
+          
+         
+           
+
+           //console.log( memoire[memoire.length - 1] == memoire[memoire.length - 2])
+
+
+
+        }
+          //console.log("true");
+
+
+
+
+
+
+        if (button.classList.contains("opperateur")) {
+          //console.log("c'est un opérateur");
+        }
+
+
+      });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  });
---
+++ test.js
@@ -0,0 +1,36 @@
+/*let c = "Baanna";
+
+for (let i = 0; i < c.length; i++) {
+    if(c[i] === "a" && c[i+2] === "a") {
+        console.log("error");
+        break;
+    
+    } 
+    console.log("azerty");
+}
+for (let i = 0; i < memoire.length; i++) {
+            if(memoire[i] === button.classList.contains("opperateur") && memoire[i+1] === button.classList.contains("opperateur")){
+              console.log("erreur");
+            } else {
+              memoire.push(nbre);
+            } 
+          }*/
+
+
+let tab = []       
+
+//console.log(tab[tab.length - 2] == undefined);
+///*console.log(tab.includes("a"));
+//console.log(tab[tab.length - 1])
+//console.log(tab.includes(tab[tab.length - 1]) == "2")
+//console.log(["+","-","x","/"].includes("x"))*/
+
+let memoire = ["2"]
+
+
+//console.log(memoire.includes(memoire[memoire.length - 1]));
+console.log(memoire[memoire.length - 1] )
+
+while (memoire > 0){
+  console.log("true")
+}
